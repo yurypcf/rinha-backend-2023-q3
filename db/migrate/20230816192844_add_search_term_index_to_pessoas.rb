dde84a7ff085c4196f3ad58a1e69950b71d795e6
Yury Cavalcante <yurypcfarias@gmail.com>
2023-08-22 16:48:42 -0300

    feat: adding pg_trgm extension to database

--- db/migrate/20230816192844_add_search_term_index_to_pessoas.rb
+++ db/migrate/20230816192844_add_search_term_index_to_pessoas.rb
@@ -3,6 +3,7 @@ class AddSearchTermIndexToPessoas < ActiveRecord::Migration[7.0]
 
   def change
     enable_extension "btree_gin"
+    enable_extension "pg_trgm"
 
     add_index :pessoas, :search_term, using: :gin, algorithm: :concurrently
   end
